6cbaaab2c794301778a77db74b766f9975533d5d
lyxabc <lyxabc@qq.com>
2024-06-05 17:11:23 +0800

    Spring

---
+++ src/main/java/com/demo/SpringLearnApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello leo!!");
+		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+
+}
